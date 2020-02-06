6d6943a087ae18127c4e58ed9c49c57e87d30117
abechoi <abraham.choi@icloud.com>
2020-02-06 01:07:09 -0800

    Finished ABOUT Section

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,33 +4,41 @@ const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
 
+
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+menuNav.addEventListener('click', closeMenu);
+
 
 function toggleMenu(){
     
     if(!showMenu){
-
-        burger.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        navItems.forEach( i => {
-            i.classList.add('open');
-        });
-
-        showMenu = true;
+        openMenu();
     }
     else{
-
-        burger.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        navItems.forEach( i => {
-            i.classList.remove('open');
-        });
-
-        showMenu = false;
-
+        closeMenu();
     }
+}
+
+function openMenu(){
+    burger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach( i => {
+        i.classList.add('open');
+    });
+
+    showMenu = true;
+}
+
+function closeMenu(){
+    burger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach( i => {
+        i.classList.remove('open');
+    });
+
+    showMenu = false;
 }
